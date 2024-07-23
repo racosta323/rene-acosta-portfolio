464416b180acf1e2c509bfa39c00f249cdeb7dda
Rene <racosta323@gmail.com>
2024-07-22 23:52:56 -0500

    adjusts font size of about me section

--- src/About.js
+++ src/About.js
@@ -1,8 +1,8 @@
 function About(){
     return (
         <div className="container">
-            <h2 className="m-0">Hi! I'm Rene.</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel enim sagittis, blandit libero pulvinar, aliquet magna. In sit amet ipsum tincidunt leo posuere posuere vitae at arcu. Fusce purus ipsum, hendrerit vitae sapien feugiat, mollis mollis magna. Curabitur malesuada dui vitae velit suscipit lobortis. Phasellus magna nisi, efficitur sit amet est vitae, semper tempus nisi. Ut scelerisque pharetra nisl, eu condimentum orci tristique id. Nam consectetur mollis consequat. Aenean pellentesque, velit sit amet pulvinar scelerisque, nulla ipsum imperdiet erat, sed dapibus risus purus in mauris. Cras et ornare urna. Praesent cursus massa eu risus suscipit pretium. Aenean porttitor aliquam laoreet.</p>
+            <h2 className="name-tag">Hi! I'm Rene.</h2>
+            <p id="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel enim sagittis, blandit libero pulvinar, aliquet magna. In sit amet ipsum tincidunt leo posuere posuere vitae at arcu. Fusce purus ipsum, hendrerit vitae sapien feugiat, mollis mollis magna. Curabitur malesuada dui vitae velit suscipit lobortis. Phasellus magna nisi, efficitur sit amet est vitae, semper tempus nisi. Ut scelerisque pharetra nisl, eu condimentum orci tristique id. Nam consectetur mollis consequat. Aenean pellentesque, velit sit amet pulvinar scelerisque, nulla ipsum imperdiet erat, sed dapibus risus purus in mauris. Cras et ornare urna. Praesent cursus massa eu risus suscipit pretium. Aenean porttitor aliquam laoreet.</p>
         </div>
     )
 }
