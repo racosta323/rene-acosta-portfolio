a86ea84d75b4dc544b40c7631dd6dd869ff568cd
Rene <racosta323@gmail.com>
2024-07-22 23:46:44 -0500

    centers on screen about section text

--- src/About.js
+++ src/About.js
@@ -1,13 +1,8 @@
 function About(){
     return (
-        <div>
-            <div>
-                <h2>Some smaller text that is centered and will be a bit more descriptive.</h2>
-            </div>
-            <div>
-                <div>one</div>
-                <div>two</div>
-            </div>
+        <div className="container">
+            <h2 className="m-0">Hi! I'm Rene.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel enim sagittis, blandit libero pulvinar, aliquet magna. In sit amet ipsum tincidunt leo posuere posuere vitae at arcu. Fusce purus ipsum, hendrerit vitae sapien feugiat, mollis mollis magna. Curabitur malesuada dui vitae velit suscipit lobortis. Phasellus magna nisi, efficitur sit amet est vitae, semper tempus nisi. Ut scelerisque pharetra nisl, eu condimentum orci tristique id. Nam consectetur mollis consequat. Aenean pellentesque, velit sit amet pulvinar scelerisque, nulla ipsum imperdiet erat, sed dapibus risus purus in mauris. Cras et ornare urna. Praesent cursus massa eu risus suscipit pretium. Aenean porttitor aliquam laoreet.</p>
         </div>
     )
 }
