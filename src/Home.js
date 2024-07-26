dc9bc436779e4c04b4fddb871116179d3a52a229
Rene <racosta323@gmail.com>
2024-07-26 11:15:35 -0500

    adds hero image to homepage

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,17 @@ import ContactButton from "./ContactButton"
 function Home(){
     return(
         <section>
-            <h1>Multimodal travel, made easy</h1>
+            <h1>Hello, I'm Rene Acosta!</h1>
             <div className="center">
                 <h2>Empower travellers and travel companies with choice, control and confidence. Snowfall's game-changing technology, Junction, makes booking and managing multimodal travel effortless.</h2>
             </div>
             <ContactButton/>
+            <img
+                src='/vecteezy_programmer-people-concept-use-laptop-and-programming-code_.jpg'
+                alt='avatar of businessman'
+                id="man-avatar"
+            />
+            <a href="https://www.vecteezy.com/free-vector/software-development">Software Development Vectors by Vecteezy</a>
         </section>
     )
 }
