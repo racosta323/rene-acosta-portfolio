b2a1907e78026c9db6ab1426a37dba45bf3e8423
Rene <racosta323@gmail.com>
2024-07-27 18:33:25 -0500

    adds a numbers section

--- src/Resume.js
+++ src/Resume.js
@@ -4,6 +4,20 @@ function Resume(){
             <div id='resume'>
                 <h3>Distribute and source all travel content through one API</h3>
             </div>
+            <div id="numbers">
+                <div>
+                    <h3>30+</h3>
+                    <p>hello</p>
+                </div>
+                <div>
+                    <h3>30+</h3>
+                    <p>hello</p>
+                </div>
+                <div>
+                    <h3>30+</h3>
+                    <p>hello</p>
+                </div>
+            </div>
             <button>Check out my resume here!</button>
         </section>
     )
