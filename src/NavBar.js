0695fb88284abb009fa6ad05452c977271b2c26b
Rene <racosta323@gmail.com>
2024-07-28 16:10:01 -0500

    lays out nav bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,18 @@
+import ContactButton from  './ContactButton'
+
 function NavBar() {
     return(
         <div id="nav-container">
-            <ul>
-                <li><a href="#about">about</a></li>
-                <li><a href="#projects">projects</a></li>
-                <li><a href="#contact">contact me</a></li>
-            </ul>
+            <div>
+                <ul>
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#projects">projects</a></li>
+                    <li><a href="#contact">contact me</a></li>
+                </ul>
+            </div>
+            <div id='nav-btn'>
+                <ContactButton/>
+            </div>
         </div>
     )
 }
