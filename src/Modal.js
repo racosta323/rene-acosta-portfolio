309b610152b0765524ec817cb35c44fda582e161
Rene <racosta323@gmail.com>
2024-07-29 11:51:33 -0500

    styles modal

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,18 @@ function Modal () {
     return(
         <div className='modal'>
             <div className= 'modal-content'>
-                <p>some text</p>
+                <span class="close">&times;</span>
+                <div id="modal-left">
+                    <h3>Showmate</h3>
+                    <h4>Summary</h4>
+                    <p>Tech used: </p>
+                    <p>TECH GOES HERE</p>
+                </div>
+                <div id="modal-right">
+                    <img
+                        src="/showmate_homepage.png"
+                    />
+                </div>
             </div>
         </div>
     )
