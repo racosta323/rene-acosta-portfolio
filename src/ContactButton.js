f4968ba5b7d377e3f7c60006bad8b18beead5f6f
Rene <racosta323@gmail.com>
2024-08-04 22:40:21 -0500

    updates email in contact me button

--- src/ContactButton.js
+++ src/ContactButton.js
@@ -2,7 +2,7 @@ function ContactButton(){
     return(
         <>
            <button type="button"> 
-                <p><a href="mailto:someone@example.com"> &#10132; Contact Me</a></p>
+                <a href="mailto:racosta323@gmail.com"> &#10132; Contact Me</a>
             </button>
         </>
     )
